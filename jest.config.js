5e75c462476715c002bf23530470a3b09f0b3455
황호선 <3262398@naver.com>
2024-09-03 15:46:38 +0900

    Update Jest configuration for Codecov

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,8 @@ const customJestConfig = {
     '^.+\\.(ts|tsx)$': 'ts-jest', // ts-jest를 사용하여 TypeScript 파일을 변환합니다.
   },
   transformIgnorePatterns: ['<rootDir>/node_modules/'], // node_modules 폴더 내 파일 변환을 무시합니다.
+  collectCoverage: true, // 커버리지 정보를 수집하도록 설정
+  coverageDirectory: 'coverage', // 커버리지 결과를 저장할 디렉토리 설정
 }
 
 module.exports = createJestConfig(customJestConfig)
